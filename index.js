58bbaeb3c09bdcd3298e996c71338a69381fd6cc
IBM-W3 <sam.elwitt@ogilvy.com>
2018-08-29 11:11:51 -0400

    add dupe warnings

--- index.js
+++ index.js
@@ -9,16 +9,42 @@ try {
     fs.unlinkSync(output);
   }
   let csv = '';
+  let metrics = [];
+  let reports = [];
+  let dimensions = [];
+  let other = [];
 
   input.forEach((record, i) => {
-    let { synonyms } = record;
+    let { synonyms, title, type } = record;
+    switch (type) {
+      case 'metric':
+        if (metrics.indexOf(record) > -1) {
+          console.error(`** duplicated metric title: ${title}`);
+        }
+        metrics.push(title);
+        break;
+      case 'report':
+        if (reports.indexOf(record) > -1) {
+          console.error(`** duplicated report title: ${title}`);
+        }
+        reports.push(title);
+        break;
+      case 'dimension':
+        if (dimensions.indexOf(record) > -1) {
+          console.error(`** duplicated dimension title: ${title}`);
+        }
+        dimensions.push(title);
+        break;
+      default: other.push(title); break;
+    }
     synonyms = synonyms.length > 0 ? `,${synonyms.replace(/,\s/g,',')}` : '';
-    csv = csv + `${record.type},${record.title}${synonyms}`;
+    csv = csv + `${type},${title}${synonyms}`;
     if (i < input.length -1) {
       csv = csv + '\n';
     }
   })
   fs.writeFileSync(output, csv, { encoding: 'utf8' });
+  console.log(`metrics: ${metrics.length}, reports: ${reports.length}, dimensions: ${dimensions.length}, other: ${other.length}`);
 } catch (err) {
   console.error(err);
 }
